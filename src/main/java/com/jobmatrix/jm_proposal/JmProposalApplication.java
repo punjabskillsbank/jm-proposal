5caa6894808907bb2fac3c4e98f498d6e3235af0
Ivkaran Singh <ivkaransingh@macbook-pro.mynetworksettings.com>
2025-04-12 11:56:39 -0400

    JMK_51: setup project

---
+++ src/main/java/com/jobmatrix/jm_proposal/JmProposalApplication.java
@@ -0,0 +1,13 @@
+package com.jobmatrix.jm_proposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmProposalApplication.class, args);
+	}
+
+}
