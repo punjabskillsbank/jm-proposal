452ad6505b2348c90f1dbe58f0fc77aa964314a2
pawanpreet193 <pawanpreetsingh193@gmail.com>
2025-07-29 18:59:24 +0530

    JMK_122: added final constants for kafka config

--- src/main/java/com/jobmatrix/jm_proposal/config/KafkaTopicConfig.java
+++ src/main/java/com/jobmatrix/jm_proposal/config/KafkaTopicConfig.java
@@ -13,13 +13,17 @@ public class KafkaTopicConfig {
     @Value("${kafka.topic.proposal-submitted}")
     private String topicName;
 
+    private static final int PARTITIONS = 1;
+    private static final int REPLICAS = 1;
+    private static final String RETENTION_MS = "604800000";
+
     @Bean
     public NewTopic topic(){
 
         return TopicBuilder.name(topicName)
-                .partitions(1)
-                .replicas(1)
-                .config(TopicConfig.RETENTION_MS_CONFIG, "604800000") // 7 days
+                .partitions(PARTITIONS)
+                .replicas(REPLICAS)
+                .config(TopicConfig.RETENTION_MS_CONFIG, RETENTION_MS) // 7 days
                 .config(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_DELETE)
                 .build();
     }
