047620035ac0c5bb9905eb36832ed255a85f361c
codebyjsa <jsaishere@gmail.com>
2025-09-04 09:55:14 +0530

    JMK_140: Shifted InvalidFileTypeException to common

--- src/main/java/com/jobmatrix/jm_proposal/exception/InvalidFileTypeException.java
+++
@@ -1,7 +0,0 @@
-package com.jobmatrix.jm_proposal.exception;
-
-public class InvalidFileTypeException extends RuntimeException{
-    public InvalidFileTypeException(String extension) {
-        super("File type not allowed: " + extension);
-    }
-}
